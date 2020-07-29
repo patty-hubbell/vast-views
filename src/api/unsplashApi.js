7bc03ffc94e2635bfb3e47da5c71547fa4a060cb
Patrick Hubbell <hubbell.64@osu.edu>
2020-07-28 20:46:30 -0400

    Refactor code  and add comments to unsplashApi file

--- src/api/unsplashApi.js
+++ src/api/unsplashApi.js
@@ -1,28 +1,39 @@
 export default function unsplashApi() {
+  // Initialize the imageOptions to the default both (Portrait and landscape)
   let imageOptions = "both";
 
+  // Declare loader and imageContainer, which will hold these elements in the document
   let loader;
   let imageContainer;
 
+  // imagesArray will hold the images in the response from the Unsplash API
+  // readyToLoadMore becomes true when images from current response are all loaded
+  // imagesLoaded is the current number of images loaded
+  // totalImages is the total number of images given in the current API response
   let imagesArray = [];
-  let ready = false;
+  let readyToLoadMore = false;
   let imagesLoaded = 0;
   let totalImages = 0;
 
-  let count = 5;
+  // Initial number of images to be requested from the API is 5 in order to
+  // reduce initial loading times
+  let imageLoadCount = 10;
+
   const apiKey = "lA_pnbXwphLuyywI4jsVsH8_o43qhajfccxqwjiW6lQ";
-  const apiUrlBoth = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}&query=nature`;
-  const apiUrlPortrait = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}&query=nature&orientation=portrait`;
-  const apiUrlLandscape = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}&query=nature&orientation=landscape`;
+
+  // Initialize API urls for each possible request (Portrait nature images, landscape, or both)
+  const apiUrlBoth = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${imageLoadCount}&query=nature`;
+  const apiUrlPortrait = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${imageLoadCount}&query=nature&orientation=portrait`;
+  const apiUrlLandscape = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${imageLoadCount}&query=nature&orientation=landscape`;
 
   // Check if all the images were loaded
   const imageLoaded = () => {
     imagesLoaded++;
 
+    // If we loaded all images from current response, we can load more
     if (imagesLoaded === totalImages) {
-      ready = true;
+      readyToLoadMore = true;
       loader.hidden = true;
-      count = 30;
     }
   };
 
@@ -56,10 +67,34 @@ export default function unsplashApi() {
     });
   };
 
+  // Empty the image container and request 5 new images corresponding to the
+  // new image option selection
+  const resetImages = () => {
+    let imageContainer = document.getElementById("image-container");
+
+    // Empty the image container
+    imageContainer.textContent = "";
+
+    // Scroll to the top of the page
+    window.scrollTo(0, 0);
+
+    // Make the loading screen visible
+    loader.hidden = false;
+
+    // Reset imageLoadCount to its intial value
+    imageLoadCount = 10;
+
+    // Request the new images from the API
+    getImages();
+  };
+
   // Get photos from the Unsplash API
   const getImages = async () => {
     try {
       let response;
+
+      // Request images from the API with the url string corresponding to the
+      // current image option selection
       if (imageOptions === "both") {
         response = await fetch(apiUrlBoth);
       } else if (imageOptions === "portrait") {
@@ -67,50 +102,55 @@ export default function unsplashApi() {
       } else if (imageOptions === "landscape") {
         response = await fetch(apiUrlLandscape);
       }
+
+      // Get the images array
       imagesArray = await response.json();
+
+      // If the imageLoadCount is set to the initial value of 5, change the
+      // value to 30
+      if (imageLoadCount === 10) {
+        imageLoadCount = 30;
+      }
+
+      // Load and display the new images
       displayImages();
-    } catch (error) {}
+    } catch (error) {
+      console.error("Error:", error);
+    }
   };
 
+  // Whenever the image options form is clicked, check to see if a new
+  // image option has been selected
   const getImageOptions = () => {
     document
       .getElementById("image-options-form")
       .addEventListener("click", () => {
-        let imageContainer = document.getElementById("image-container");
+        // If the current checked image option does not match the
+        // previously recorded selection, we know a new option has been
+        // selected, and we need to reset our image container
         if (
           document.getElementById("portrait").checked &&
           imageOptions !== "portrait"
         ) {
           imageOptions = "portrait";
-          imageContainer.textContent = "";
-          window.scrollTo(0, 0);
-          loader.hidden = false;
-          count = 5;
-          getImages();
+          resetImages();
         } else if (
           document.getElementById("landscape").checked &&
           imageOptions !== "landscape"
         ) {
           imageOptions = "landscape";
-          imageContainer.textContent = "";
-          window.scrollTo(0, 0);
-          loader.hidden = false;
-          count = 5;
-          getImages();
+          resetImages();
         } else if (
           document.getElementById("both").checked &&
           imageOptions !== "both"
         ) {
           imageOptions = "both";
-          imageContainer.textContent = "";
-          window.scrollTo(0, 0);
-          loader.hidden = false;
-          count = 5;
-          getImages();
+          resetImages();
         }
       });
   };
 
+  // Define each variable that holds a document element
   const getDocumentElements = () => {
     loader = document.getElementById("loader");
     imageContainer = document.getElementById("image-container");
@@ -121,9 +161,12 @@ export default function unsplashApi() {
     if (
       window.innerHeight + window.scrollY >=
         document.body.offsetHeight - 1000 &&
-      ready
+      readyToLoadMore
     ) {
-      ready = false;
+      // Set readyToLoadMore to false since 30 new images must be loaded
+      readyToLoadMore = false;
+
+      // Request the 30 new images
       getImages();
     }
   });
