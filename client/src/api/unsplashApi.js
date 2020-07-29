1f34a2692ec64e319326f06d706d97f710d83aad
Patrick Hubbell <hubbell.64@osu.edu>
2020-07-29 18:57:59 -0400

    Change apiUrls

--- client/src/api/unsplashApi.js
+++ client/src/api/unsplashApi.js
@@ -20,9 +20,9 @@ export default function unsplashApi() {
   let imageLoadCount = 10;
 
   // Initialize API urls for each possible request (Portrait nature images, landscape, or both)
-  const apiUrlBoth = `http://localhost:5000/api/random/both/${imageLoadCount}`;
-  const apiUrlPortrait = `http://localhost:5000/api/random/portrait/${imageLoadCount}`;
-  const apiUrlLandscape = `http://localhost:5000/api/random/landscape/${imageLoadCount}`;
+  const apiUrlBoth = `/api/random/both/${imageLoadCount}`;
+  const apiUrlPortrait = `/api/random/portrait/${imageLoadCount}`;
+  const apiUrlLandscape = `/api/random/landscape/${imageLoadCount}`;
 
   // Check if all the images were loaded
   const imageLoaded = () => {
