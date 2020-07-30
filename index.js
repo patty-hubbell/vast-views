20e8cb8443215bdcdfcfbd90d2893db421d006bc
Patrick Hubbell <hubbell.64@osu.edu>
2020-07-30 01:27:46 -0400

    Prepare for heroku deploy

--- index.js
+++ index.js
@@ -22,10 +22,6 @@ app.use(limiter);
 // Allow CORS from any origin
 app.use(cors());
 
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
-
 app.get("/api/random/:orientation/:count", async (req, res) => {
   try {
     const orientation = req.params.orientation;
@@ -55,6 +51,16 @@ app.get("/api/random/:orientation/:count", async (req, res) => {
     });
   }
 });
+
+if (process.env.NODE_ENV === "production") {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, "client/build")));
+
+  // Handle React routing, return all requests to React app
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
